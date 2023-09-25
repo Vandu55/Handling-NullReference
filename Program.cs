f66f30ac2466ebeb0b284ab897cd35c127ed0c6e
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-26 00:12:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handling_NullReference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = null;
+                Console.WriteLine(name.Length);
+                Console.ReadLine();
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("String is Null.........");   
+            }
+        }
+    }
+}
